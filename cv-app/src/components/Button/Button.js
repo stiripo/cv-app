f4d77b253b6ee609dbfeaf1347c28cccc68789f8
Polina_Berezhneva <stiripo13@gmail.com>
2022-03-03 00:57:46 -0500

    Fix Button component after merge conflicts

--- cv-app/src/components/Button/Button.js
+++ cv-app/src/components/Button/Button.js
@@ -4,7 +4,7 @@ export function Button(props) {
     const size = props.size;
     return (
         <>
-            <button className={`${styles.button} ${size === 'mini' ? styles.mini : ''}`}>
+            <button className={`${styles.button} ${props.className} ${size === 'mini' ? styles.mini : ''}`} onClick={props.onClick} disabled={props.disabled}>
                 <span>{props.icon}</span>
                 <span className={styles.button_text}>{props.text}</span>
             </button>
